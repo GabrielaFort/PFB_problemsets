817c93c904f5a7749c93a2367227ebc37ed914fc
pfb20 <pfb20@info20.cshl.edu>
2023-10-17 11:48:44 -0400

    adding first python script! Woohoo

---
+++ day2/first_python_script.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+
+name = sys.argv[1] #Setting up four different arguments for this script - will need to type them in this order
+fav_color = sys.argv[2]
+fav_activity = sys.argv[3]
+fav_animal = sys.argv[4]
+
+print("This script takes four inputs - your name, your favorite color, your favorite activity, and your favorite animal")
+
+if len(sys.argv) > 5:
+	print("ERROR: Include only four arguments - name, fav color, fav activity, and fav animal")
+	exit()
+
+
+print(f"My name is:{name}\nMy favorite color is:{fav_color}\nMy favorite activity is:{fav_activity}\nMy favorite animal is:{fav_animal}")
+
